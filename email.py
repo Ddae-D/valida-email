4c6eb3c69e486c3195f40cd1e283b9fa32af5ff0
Ddae-D <87243869+Ddae-D@users.noreply.github.com>
2021-07-15 13:40:26 -0500

    validamos vacíos y no inicio con numéricos

--- email.py
+++ email.py
@@ -1,14 +1,40 @@
-class EmailValidate(): #class
+import numpy as np
+import math as mt
+#------------------------------------------------------------------------------------------------
+class Welcome():
+    def __init__(self):
+        self.titleMain= '*** validacion de correo electronico ***'
+        
+    def welcome(self):
+        print("****************************************")
+        print(self.titleMain.title())#convertion inline of text special for search
+        print("****************************************")
+#------------------------------------------------------------------------------------------------
+class EmailValidate(): #class 
+        
     def __init__(self,arg): #constructor
         self.__mail = arg #arg in self.__email
 
     def __controlEmail(self): #function private
-        print(self.__mail)
+        print("*** email validating ***")
+        try:
+            if((" " in self.__mail) or ((self.__mail[0].isdigit() != False))):
+                print("Error en la escritura del correo")        
+            print("your email is :", self.__mail)
+        except Exception as e:
+            print("*** exception is : ", e , " ***")
 
-    def testEmail(self): #function public, tell to system, give me to __imprime
+    def controlEmail(self): #function public, tell to system, give me to __controlEmail
         self.__controlEmail()
-#
-#
-arg = input("enter you email: ") #recept email
+#------------------------------------------------------------------------------------------------
+"""class Test(Welcome):
+    print("Ejemplo clase Test")"""
+    
+#------------------------------------------------------------------------------------------------
+w = Welcome().welcome()
+arg = input("*** enter your email: ").casefold()   
+e = EmailValidate(arg).controlEmail()
+"""arg = input("enter you email: ") #recept email
 emaile = EmailValidate(arg) #sent email to constructor
 emaile.testEmail() #start function hehe
+"""
